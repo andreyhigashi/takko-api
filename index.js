3d7482d0457641e5761f2d6b844f9dc162a88873
andreyhigashi <andreyhigashi@gmail.com>
2026-04-26 18:44:52 -0300

    fix cors properly

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const anunciosPath = path.join(__dirname, 'anuncios.json');
 app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
